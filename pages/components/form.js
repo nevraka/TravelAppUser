e74e9c09cec2f4cdd8369a0f375f492971a98057
nevraka <nevrakaya@gmail.com>
2020-11-17 23:03:53 +0100

    form screen changed

--- pages/components/form.js
+++ pages/components/form.js
@@ -2,13 +2,18 @@ import styles from '../../styles/Homescreen.module.css';
 
 function Form() {
   return (
-    <div className={styles.search}>
-      <div className={styles.origin}>a</div>
-      <div className={styles.destination}>b</div>
-      <div className={styles.dates}>c</div>
-      <div className={styles.passengers}>s</div>
-      <div className={styles.submit}>ç</div>
-    </div>
+    <>
+      <div className={styles.search}>
+        <div className={styles.origin}>
+          <input type="text" placeholder="Your destination/hotel"></input>
+        </div>
+        <div className={styles.checkin}>c</div>
+        <div className={styles.checkout}>s</div>
+        <div className={styles.guest}>s</div>
+        <div className={styles.submit}> Find Hotel 🏩</div>
+      </div>
+      <div className={styles.multisearch}>🧩 Multi-city search</div>
+    </>
   );
 }
 
