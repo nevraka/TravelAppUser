42b679bbe1c092d5e8ae90d8adea5755eb0ee86d
nevraka <nevrakaya@gmail.com>
2020-11-17 23:02:46 +0100

    created usecontext

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,24 @@
 import styles from '../styles/Homescreen.module.css';
 import Header from './components/header';
 import Form from './components/form';
+import { ThemeProvider } from './ThemeContext';
 
 function Homescreen() {
   return (
     <>
-      <Header />
-      <div className={styles['header-title']}>
-        <h1 className={styles.banner}>Search for cheap hotels</h1>
-        <div className={styles.minibanner}>
-          The best way to buy cheap hotels
+      <ThemeProvider>
+        <Header />
+        <div className={styles['header-title']}>
+          <h1 className={styles.banner}>Search for cheap hotels</h1>
+          <div className={styles.minibanner}>
+            The best way to buy cheap hotels
+          </div>
+          <div className={styles['active-hotel']}>
+            <span>HOTELS</span>
+          </div>
         </div>
-        <div className={styles['active-hotel']}>
-          <span>HOTELS</span>
-        </div>
-      </div>
-      <Form />
+        <Form />
+      </ThemeProvider>
     </>
   );
 }
