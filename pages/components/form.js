3b99da1e1b9fe6254c58ea691e1f4fd12ce06e61
nevraka <nevrakaya@gmail.com>
2020-11-19 17:34:52 +0100

    added guestinput component

--- pages/components/form.js
+++ pages/components/form.js
@@ -1,19 +1,37 @@
 import styles from '../../styles/Homescreen.module.css';
+import { Input, Cascader, Button, Divider } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+import GuestInputs from './GuestInputs';
 
 function Form() {
-  return (
-    <>
-      <div className={styles.search}>
-        <div className={styles.origin}>
-          <input type="text" placeholder="Your destination/hotel"></input>
+  function dropdownRender() {
+    return (
+      <div style={{ width: '200px' }}>
+        <div>
+          <GuestInputs />
         </div>
-        <div className={styles.checkin}>c</div>
-        <div className={styles.checkout}>s</div>
-        <div className={styles.guest}>s</div>
-        <div className={styles.submit}> Find Hotel 🏩</div>
+        <Divider style={{ margin: 5 }} />
+        <div style={{ padding: 8 }}>Age of children</div>
       </div>
-      <div className={styles.multisearch}>🧩 Multi-city search</div>
-    </>
+    );
+  }
+
+  return (
+    <div className={styles.search}>
+      <Input placeholder="Your Destination/hotel" className={styles.origin} />
+      <Input placeholder="Check-in date " className={styles.checkin} />
+      <Input placeholder="Check-out date" className={styles.checkout} />
+      <Cascader
+        placeholder="Guest"
+        bordered={false}
+        dropdownRender={dropdownRender}
+        className={styles.guest}
+      />
+      <Button className={styles.submit} icon={<SearchOutlined />}>
+        Find Hotel
+      </Button>
+      <span className={styles.multisearch}>🧩 Multi-city search</span>
+    </div>
   );
 }
 
